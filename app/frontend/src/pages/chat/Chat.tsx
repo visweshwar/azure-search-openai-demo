5f97b746ba14798080e0f94244c1c9965e09b9ab
Eirik Haughom <eirik@haughom.net>
2023-06-08 19:37:25 +0000

    update

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -181,7 +181,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. what are the transportation methods available to the site?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
