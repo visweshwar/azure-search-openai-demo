04f531531d979ccb762e9f431e02d85e03a5b133
Eirik Haughom <eirik@haughom.net>
2023-06-08 19:15:56 +0000

    update

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>SMR Hackathon by Eirik</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -25,7 +25,7 @@ const Layout = () => {
                                 </NavLink>
                             </li>
                             <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                <a href="https://github.com/EirikHaughom" target={"_blank"} title="Github repository link">
                                     <img
                                         src={github}
                                         alt="Github logo"
